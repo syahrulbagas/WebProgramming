a2f48d765dd5933d06751d8cc049db7eaa06fe3c
Syahrul Bagas <71810571+syahrulbagas@users.noreply.github.com>
2022-09-24 17:01:14 +0700

    Add files via upload
    CRUD

---
+++ Labwork8/read.php
@@ -0,0 +1,60 @@
+<?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
+$records_per_page = 5;
+
+
+$stmt = $pdo->prepare('SELECT * FROM mahasiswa ORDER BY id LIMIT :current_page, :record_per_page');
+$stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
+$stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
+$stmt->execute();
+$contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$num_contacts = $pdo->query('SELECT COUNT(*) FROM mahasiswa')->fetchColumn();
+?>
+
+
+<?=template_header('Read')?>
+
+<div class="content read">
+	<h2>Mahasiswa</h2>
+	<a href="create.php" class="create-contact">Tambah Data</a>
+	<table>
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>Nama Kelas</td>
+                <td>Dosen</td>
+                <td>SKS</td>
+                <td>Deskripsi</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($contacts as $contact): ?>
+            <tr>
+                <td><?=$contact['id']?></td>
+                <td><?=$contact['nama_kelas']?></td>
+                <td><?=$contact['dosen']?></td>
+                <td><?=$contact['sks']?></td>
+                <td><?=$contact['deskripsi']?></td>
+                <td class="actions">
+                    <a href="update.php?id=<?=$contact['id']?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
+                    <a href="delete.php?id=<?=$contact['id']?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+	<div class="pagination">
+		<?php if ($page > 1): ?>
+		<a href="read.php?page=<?=$page-1?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
+		<?php endif; ?>
+		<?php if ($page*$records_per_page < $num_contacts): ?>
+		<a href="read.php?page=<?=$page+1?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
+		<?php endif; ?>
+	</div>
+</div>
+
+<?=template_footer()?>
